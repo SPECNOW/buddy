d390a0bffba46459e3fac328623fe2ae3903bed6
clarkhsieh <cha55@sfu.ca>
2014-09-10 21:07:18 -0700

    back update
    merged some changed.

--- firmware/Arduino-Sensor-motor-controller-code/sketch_aug21a/sketch_aug21a.ino
+++ firmware/Arduino-Sensor-motor-controller-code/sketch_aug21a/sketch_aug21a.ino
@@ -50,16 +50,15 @@ void setup () {
   //serial shit
   Serial.begin(9600);
   Serial1.begin(38400);  
-  //encoder_setup(true);
-  //encoder_interupt_attach (true);
+  encoder_setup(true);
+  if (!encoder_interupt_attach (true) ){
+    Serial.println("Error With attachting interrupt");
+  }
+  Serial.println("Done Setup: Starting Code");
 }
 
 void loop () {
-  while(1) {
-   
-    Serial1.write(70);
 
-  }
 }
 
 int comm()
@@ -185,11 +184,8 @@ void ISR_acquire_dir_right (void) {
 	dir_right.prev = dir_right.curr;
 	dir_right.curr = digitalRead (encoder_inputA1) * 2 + digitalRead (encoder_inputA1);           // Convert binary input to decimal value
 	dir_right.out = QEM [dir_right.prev * 4 + dir_right.curr]; // out variable returns -1
-        if(Serial.available())
-        {
-          Serial.println("Right");
-          Serial.println(dir_right.out);
-        }
+        Serial.println("Right");
+        Serial.println(dir_right.out);
 }
 
 //ISR
@@ -198,11 +194,8 @@ void ISR_acquire_dir_left (void) {
 	dir_left.prev = dir_left.curr;
 	dir_left.curr = digitalRead (encoder_inputA2) * 2 + digitalRead (encoder_inputA2);           // Convert binary input to decimal value
 	dir_left.out = QEM [dir_left.prev * 4 + dir_left.curr]; // out variable returns -1
-        if(Serial.available())
-        {
-          Serial.println("Left");
-          Serial.println(dir_left.out);
-        }
+        Serial.println("Left");
+        Serial.println(dir_left.out);
 }
 
 //Function for attaching/ detaching  interupts
