b494eb124d39325b3e11a825aac9f505a5ae9892
Clark Hsieh <hsiehclark@gmail.com>
2015-10-14 22:30:53 -0700

    cosmetic changes

--- firmware/Buddy_Firmware_Hercules/include/ir_adc.h
+++ firmware/Buddy_Firmware_Hercules/include/ir_adc.h
@@ -14,6 +14,7 @@ typedef struct
 	uint16_t adc_pin;		//ADC in pin used for reading the voltage
 	uint16_t checking_duration;	//Variable used to keep track of checking frequency
 	uint16_t _last_adc_value;	//ADC value from the sensor
+	boolean _adc_conversion_active;	//Flag for being in the conversion list
 	boolean _adc_conversion_busy;	//Busy flag
 }ir_sensor;
 
@@ -26,5 +27,5 @@ typedef struct
 
 float getDistance_IR(ir_sensor * sensor);	//Calculate fromt he adc value and return the distance in mm 
 void addIRSensor(ir_sensor * sensor);		//Adds a sensor to the IR array
-int addIRConversion(ir_sensor * sensor);		//Add the sensor to the list to be ADC converted
+int addIRConversion(ir_sensor * sensor);	//Add the sensor to the list to be ADC converted
 int removeIRConversion(ir_sensor *sensor);	//Remove the IR sensor from the ADC conversion list
