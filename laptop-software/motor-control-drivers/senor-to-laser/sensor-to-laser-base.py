1de3f7fb52fd4e1121218691c5db0a1f869f80d6
Clark Hsieh <clark@specbot.(none)>
2016-04-06 21:44:19 -0700

    - changed the sensor-to-laser-base.py to be a class that can be imported

--- laptop-software/motor-control-drivers/senor-to-laser/sensor-to-laser-base.py
+++ laptop-software/motor-control-drivers/senor-to-laser/sensor-to-laser-base.py
@@ -4,25 +4,37 @@ import rospy
 
 from sensor_msgs.msg import LaserScan
 
-def LaserData(node_name, node_topic, publish_rate):
-    rospy.init_node(node_name)
-    rospy.is_shutdown()
-    rate = rospy.Rate(publish_rate)
-    pub = rospy.Publisher(node_topic, LaserScan, queue_size=5)
-    while not rospy.is_shutdown():
-        (ranges, intensities) = getData()
-        FakeLaserScan= LaserScan(ranges=ranges, intensities=intensities)
-        pub.publish(FakeLaserScan)
-        rate.sleep()
+class sensor_to_laser_base:
+    def __init__(self, node_name, node_topic, publish_rate, queue_size):
+        """
+        Function for initializing 
+        """
+        rospy.init_node(node_name)
+        rospy.is_shutdown()
+        self.rate = rospy.Rate(publish_rate)
+        self.pub = rospy.Publisher(node_topic, LaserScan, queue_size=queue_size)
 
-def getData(num_samples=10):
-    ranges = [random.random()*10 for i in range(num_samples)]
-    intensities = [1 for i in range(num_samples)]
-    return ranges, intensities
+    def startPublishing(self):
+        """
+        start publishing the data
+        """
+        while not rospy.is_shutdown():
+            (ranges, intensities) = self.getData()
+            FakeLaserScan= LaserScan(ranges=ranges, intensities=intensities)
+            self.pub.publish(FakeLaserScan)
+            self.rate.sleep()
+
+    def getData(self,num_samples=10):
+    	"""
+    	This getData function needs to be overloaded, else default it will work
+    	"""
+        ranges = [random.random()*10 for i in range(num_samples)]
+        intensities = [1 for i in range(num_samples)]
+        return ranges, intensities
 
 if __name__=='__main__':
     try:
-        LaserData('FakeLaserDataNode', 'FakeLaserTopic', 10)
+        LaserData('FakeLaserDataNode', 'FakeLaserTopic', 10, 10)
     except rospy.ROSInterruptException:
         pass
 
