d8142b20da48664ef03db27d7e3c0a321ed8c4eb
charanpreetp <charanpreetp@gmail.com>
2016-06-01 21:57:14 -0700

    * Updated Serial Node so that data is correctly extracted from serial data messages

--- laptop-software/src/motor_control_drivers/src/serial_node.py
+++ laptop-software/src/motor_control_drivers/src/serial_node.py
@@ -1,37 +1,35 @@
 import serial
+import struct
+
 def main():
   ser = initserial()
-  ser.open()
   ser.write("sr")
   ser.read()
-  ser.write("sr")
-  ser.open()
-  ser.open()
-  ser.port = '/dev/ttyUSB1'
-  ser.open()
-  ser.write("sr")
-  ser.read()
+  serialprocess(ser.read(18))
 
-def initserial():
+def initserial(port = '/dev/ttyUSB0'):
   ser = serial.Serial()
   ser.baurdrate = 9600
-  ser.port = '/dev/ttyUSB0'
+  ser.port = port
   ser.open()
   return ser
   
 def serialprocess(serialbuffer):
   Packetheader = ord(serialbuffer[0])
-  ValiData = ord(serialbuffer[1])
-  EncL = stringtofloat(serialbuffer[2:5])
-  EncR = stringtofloat(serialbuffer[6:9])
-  UltraF = ord(serialbuffer[10])
-  UltraB = ord(serialbuffer[11])
+  ValidData = ord(serialbuffer[1])
+  UltraF = ord(serialbuffer[2])
+  UltraB = ord(serialbuffer[3])
+  # serialbuffer[2:3] are 0x00,0x00 for some reason...
+  EncL = stringtofloat(serialbuffer[4:8])
+  EncR = stringtofloat(serialbuffer[8:12])
   infra = [ord(serialbuffer[12]), 
 	   ord(serialbuffer[13]), 
 	   ord(serialbuffer[14]), 
 	   ord(serialbuffer[15]), 
 	   ord(serialbuffer[16]), 
 	   ord(serialbuffer[17])]
+  return Packetheader, ValidData, EncL, EncR, UltraF, UltraB, infra
  
 def stringtofloat(string):
-  return float(ord(string[0])<< 3 + ord(string[1]) << 2 + ord(string[2]) << 1 + ord(string[3]))
+  return struct.unpack('>f', string[::-1])[0]
+  
