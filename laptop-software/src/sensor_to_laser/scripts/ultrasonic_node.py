744c335b8787aefe3b3a62aa13c259b511e03a97
Clark Hsieh <cha55@sfu.alumia.ca>
2016-08-03 22:24:54 -0700

    Startrted Ultrasonic"

---
+++ laptop-software/src/sensor_to_laser/scripts/ultrasonic_node.py
@@ -0,0 +1,12 @@
+#!/user/bin/python
+import rospy
+
+
+#from sensor_msg.msg import LaserScan
+
+imoport sensor_to_laser_base
+
+print "Ultrasonic node"
+
+ultra_obj=sensor_to_laser_base(ultrasonic_node,
+
