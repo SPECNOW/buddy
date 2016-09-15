5c7269d7e8208e9588808c28cbbf4be2baf36e1c
Clark Hsieh <cha55@sfu.alumia.ca>
2016-09-14 21:54:41 -0700

    Updated: new design, work in progress, file not run able yet

--- laptop-software/src/sensor_to_laser/scripts/sensor_to_laser_base.py
+++ laptop-software/src/sensor_to_laser/scripts/sensor_to_laser_base.py
@@ -4,23 +4,15 @@ import rospy
 
 """
 CP Aug 31, 2016
-
+updated sept 14th
 This doesn't work, but it looks roughly like what we
 want it to look like.
+
+Design:
+1) use launch file to launch X number copies of this file with params to specify which sensor etc etc
 """
 from sensor_msgs.msg import LaserScan
 from motor_control_drivers.msg import BuddySerial
-
-        """
-        rospy.init_node(node_name)
-        rospy.is_shutdown()
-        self.sensor_array = []
-        for sensor_number in range(number_of_sensors):
-            self.sensor_array.append(
-                sensor_to_laser_base()
-            )
-
-       """
 
 class sensor_to_laser_base:
     def __init__(self, node_topic, queue_size, publish_rate, number_of_datapoints=10):
@@ -29,11 +21,21 @@ class sensor_to_laser_base:
 	* create a publisher instance
         * each instance should be 
         """
-        self.data = None
-        self.number_of_datapoints = number_of_datapoints
+	self.data_source_topic # = rospy.get_param(someParam)
+	self.sensor_number # = rospy.get_param(someParam)
+	self.publish_topic # = rospy.get_param(someParam) + sensor_number
+	self.sensor_width_angle # = rospy.get_param(someParam)
+	self.sensor_min_value # = rospy.get_param(someParam)
+	self.sensor_max_value # = rospy.get_param(someParam)
+	self.sensor_scale_factor # = rospy.get_param(someParam)
+	self.valid_data_mask # = rospy.get_param(someParam)
+
+        rospy.init_node(node_name)
+	self.sub = rospy.Subscriber(data_source_topic, BuddySerial, proccessData)
         self.rate = rospy.Rate(publish_rate)
-        self.pub = rospy.Publisher(node_topic, LaserScan, queue_size=queue_size)
-        return
+        self.pub = rospy.Publisher(publish_topic, LaserScan, queue_size=queue_size)
+        # rospy.is_shutdown() # Does this do anything?
+	return
 
     def startPublishing(self):
         """
@@ -47,13 +49,6 @@ class sensor_to_laser_base:
             self.rate.sleep()
         return
 
-    def setData(self, data):
-        """
-        for the controller to set the data
-        """
-        self.data = data
-        return
-
     def getData_laserScan(self,num_samples=10):
     	"""
     	This getData function needs to be overloaded, else default it will work
@@ -61,6 +56,12 @@ class sensor_to_laser_base:
         ranges = [random.random()*10 for i in range(self.number_of_datapoints)]
         intensities = [1 for i in range(self.number_of_datapoints)]
         return ranges, intensities
+    
+    def processData(self, data):
+        # CHeck if interested data has changed
+	if data.ValidData && self.valid_data_mask:
+	    # Extarct Data, convert Data, and Publish Data
+	return
 
 if __name__=='__main__':
     try:
