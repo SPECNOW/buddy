a81cb4f0d06a73456227a5a67b6e596abed09c9a
Charanpreet Parmar <charanpreetp@gmail.com>
2017-11-07 21:34:11 -0800

    Enabled Serial Loopback: * Created Serial Initialization function * Made main function act as serial loopback for rxed serial data * works with arduino test code!! :D

--- firmware/Buddy_delfino/Buddy/pin_configuration.h
+++ firmware/Buddy_delfino/Buddy/pin_configuration.h
@@ -13,15 +13,20 @@
 
 /************** SCI/UART **********/
 // To PC
-#define ToPC_Uart_Rx_Pin 85
-#define ToPC_Uart_Rx_PinConfig GPIO_85_SCIRXDA
-
+#define ToPC_Uart SCIA_BASE
 #define ToPC_Uart_Tx_Pin 84
 #define ToPC_Uart_Tx_PinConfig GPIO_84_SCITXDA
 
+#define ToPC_Uart_Rx_Pin 85
+#define ToPC_Uart_Rx_PinConfig GPIO_85_SCIRXDA
+
 // To Sabertooth
+#define ToSabertooth_Uart SCIA_BASE
 #define ToSabertooth_Uart_Tx_Pin 18
 #define ToSabertooth_Uart_Tx_PinConfig GPIO_18_SCITXDB
+
+#define ToSabertooth_Uart_Rx_Pin 19
+#define ToSabertooth_Uart_Rx_PinConfig GPIO_19_SCIRXDB
 
 /************** EQEP **********/
 #define LeftMotor_EQUEP_A_Pin 10
@@ -35,9 +40,5 @@
 
 #define RightMotor_EQUEP_B_Pin 63
 #define RightMotor_EQUEP_B_PinConfig GPIO_63_EQEP3B
-
-/************** ADC **********/
-
-
 
 #endif /* PIN_CONFIGURATION_H_ */
