ec5e886b88086058f6eff3b068f0d60c32bb922f
Charanpreet Parmar <charanpreetp@gmail.com>
2017-12-12 21:48:35 -0800

    Began adding test for Serial TX: * Restructured code a bit

--- firmware/Buddy_delfino/Buddy/defines.h
+++ firmware/Buddy_delfino/Buddy/defines.h
@@ -12,6 +12,15 @@
 #include "device.h"
 #include "pin_configuration.h"
 
+typedef enum DEVICE_MODE {
+    normal,
+    test_rx,
+    test_tx
+} DeviceMode;
+
+volatile extern DeviceMode deviceMode;
+
+
 void SCI_Init();
 void EQEP_Init();
 void ADC_Init();
