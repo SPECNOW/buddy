392b1fc9986d3594546181acc03e6ce42785dc2b
kaazi <lee.izaak@gmail.com>
2018-01-09 21:32:19 -0800

    Arduino Serial Receive Test

---
+++ firmware/Buddy_delfino/Buddy/ArduinoTest/ArduinoSerialRecieve/ArduinoSerialRecieve/ArduinoSerialRecieve.ino
@@ -0,0 +1,18 @@
+char incomingByte = 0;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  while(!Serial);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial1.available()>0){
+    incomingByte = Serial1.read();
+
+    Serial.print("I received: ");
+    Serial.println(incomingByte);
+  }
+  
+}
