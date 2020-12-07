3ff1f860ae68be3945b7be8599057c8514c67c23
Charanpreet Parmar <charanpreetp@gmail.com>
2020-12-06 22:47:11 -0800

    * Initialize GPIOs

--- firmware/Buddy_delfino/Buddy/defines.c
+++ firmware/Buddy_delfino/Buddy/defines.c
@@ -41,13 +41,33 @@ void ADC_Init() {
 }
 
 void GPIO_Init() {
-    /*GPIO_setPadConfig(86, GPIO_PIN_TYPE_PULLUP);     // Enable pullup on GPIO9
-    GPIO_writePin(86, 0);                            // Load output latch
-    GPIO_setPinConfig(GPIO_86_GPIO86);                // GPIO9 = GPIO9
-    GPIO_setDirectionMode(86, GPIO_DIR_MODE_OUT);    // GPIO9 = output
+    GPIO_setPadConfig(71, GPIO_PIN_TYPE_PULLUP);    // Enable pullup on GPIO71
+    GPIO_writePin(71, 0);                           // Load output latch
+    GPIO_setPinConfig(GPIO_71_GPIO71);              // GPIO71 = GPIO71
+    GPIO_setDirectionMode(71, GPIO_DIR_MODE_OUT);   // GPIO71 = output
 
-    GPIO_setPadConfig(87, GPIO_PIN_TYPE_PULLUP);     // Enable pullup on GPIO9
-    GPIO_writePin(87, 0);                            // Load output latch
-    GPIO_setPinConfig(GPIO_87_GPIO87);                // GPIO9 = GPIO9
-    GPIO_setDirectionMode(87, GPIO_DIR_MODE_OUT);    // GPIO9 = output*/
+    GPIO_setPadConfig(20, GPIO_PIN_TYPE_PULLUP);    // Enable pullup on GPIO9
+    GPIO_setPinConfig(GPIO_20_GPIO20);              // GPIO20 = GPIO20
+    GPIO_setDirectionMode(20, GPIO_DIR_MODE_IN);    // GPIO20 = input
+
+    GPIO_setPadConfig(78, GPIO_PIN_TYPE_PULLUP);    // Enable pullup on GPIO78
+    GPIO_setPinConfig(GPIO_78_GPIO78);              // GPIO78 = GPIO78
+    GPIO_setDirectionMode(78, GPIO_DIR_MODE_IN);    // GPIO78 = input
+
+    GPIO_setPadConfig(21, GPIO_PIN_TYPE_PULLUP);    // Enable pullup on GPIO21
+    GPIO_setPinConfig(GPIO_21_GPIO21);              // GPIO21 = GPIO21
+    GPIO_setDirectionMode(21, GPIO_DIR_MODE_IN);    // GPI21 = input
+
+    GPIO_setPadConfig(61, GPIO_PIN_TYPE_PULLUP);    // Enable pullup on GPIO61
+    GPIO_setPinConfig(GPIO_61_GPIO61);              // GPIO61 = GPIO61
+    GPIO_setDirectionMode(61, GPIO_DIR_MODE_IN);    // GPIO61 = input
+
+    GPIO_setPadConfig(43, GPIO_PIN_TYPE_PULLUP);    // Enable pullup on GPIO43
+    GPIO_setPinConfig(GPIO_43_GPIO43);              // GPIO43 = GPIO43
+    GPIO_setDirectionMode(43, GPIO_DIR_MODE_IN);    // GPIO43 = input
+
+    GPIO_setPadConfig(42, GPIO_PIN_TYPE_PULLUP);    // Enable pullup on GPIO42
+    GPIO_writePin(42, 0);                           // Load output latch
+    GPIO_setPinConfig(GPIO_42_GPIO42);              // GPIO42 = GPIO42
+    GPIO_setDirectionMode(42, GPIO_DIR_MODE_OUT);   // GPIO42 = output
 }
