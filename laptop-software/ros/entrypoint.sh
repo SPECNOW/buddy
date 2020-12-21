83e5905576a2325aaa4bfe38e473983023b4af2e
Charanpreet Parmar <charanpreetp@gmail.com>
2020-12-20 19:58:26 -0800

    * Running simulation, params need tweaks

--- laptop-software/ros/entrypoint.sh
+++ laptop-software/ros/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 source /opt/ros/kinetic/setup.bash
-source /home/buddy/ros/devel/setup.bash
-cd /home/buddy/ros/
+source /home/$USER/ros/devel/setup.bash
+cd /home/$USER/ros/
 catkin_make
-sleep 10000
 roslaunch src/buddy_launch.launch
